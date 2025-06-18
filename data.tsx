159007fee341f8b5495332b411076cad0fa0eefe
Shravya S Mogaveera <160824624+Shravyamogaveera19@users.noreply.github.com>
2025-06-18 20:46:53 +0530

    Create data.tsx

---
+++ data.tsx
@@ -0,0 +1,100 @@
+export const data: object[] = [
+  {
+    id: '10001',
+    label: 'Renumeration paid',
+    type: '',
+    include: '',
+    y2022: '',
+    y2023: '',
+    PageNo: '',
+    subtasks: [
+      {
+        id: '10002',
+        label: 'Key Management',
+        type: 'Expense',
+        include: 'Not Include',
+        y2022: '',
+        y2023: '',
+        PageNo: '',
+        subtasks: [
+          {
+            id: '10011',
+            label: 'DIVYAKANT ZAVERI',
+            type: '',
+            include: '',
+            y2022: '',
+            y2023: '',
+            PageNo: '12',
+          },
+          {
+            id: '10012',
+            label: 'DEBAR DUTTAGUPTA',
+            type: '',
+            include: '',
+            y2022: '101.37',
+            y2023: '101.37',
+            PageNo: '12',
+          }
+        ]
+      },
+      {
+        id: '10003',
+        label: 'SATARUPA MUKHERJEE',
+        type: '',
+        include: '',
+        y2022: '101.37',
+        y2023: '101',
+        PageNo: '12',
+      }
+    ]
+  },
+  {
+    id: '10005',
+    label: 'Director Sitting Fees',
+    type: '',
+    include: '',
+    y2022: '',
+    y2023: '',
+    PageNo: '',
+    subtasks: [
+      {
+        id: '10006',
+        label: 'Key Management Personnel (KMP)',
+        type: 'Expense',
+        include: 'Not Include',
+        y2022: '',
+        y2023: '',
+        PageNo: '',
+        subtasks: [
+          {
+            id: '10014',
+            label: 'DIVYAKANT ZAVERI',
+            type: '',
+            include: '',
+            y2022: '',
+            y2023: '',
+            PageNo: '12',
+          },
+          {
+            id: '10015',
+            label: 'DEBAR DUTTAGUPTA',
+            type: '',
+            include: '',
+            y2022: '101.37',
+            y2023: '101.37',
+            PageNo: '12',
+          }
+        ]
+      },
+      {
+        id: '10016',
+        label: 'PREYAL SHAH',
+        type: '',
+        include: '',
+        y2022: '101.37',
+        y2023: '11.37',
+        PageNo: '12',
+      }
+    ]
+  }
+];
